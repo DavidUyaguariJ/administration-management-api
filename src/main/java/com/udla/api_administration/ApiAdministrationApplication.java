b9dae585cf2893a7e8074caf2f9ee215d8a26321
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2025-11-16 22:32:52 -0500

    feat: configuraciones iniciales

---
+++ src/main/java/com/udla/api_administration/ApiAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.udla.api_administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udla/api_administration/ApiAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udla.api_administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
